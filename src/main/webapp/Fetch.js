a2da36fa7e4d4be1dba5246bbd8bf2380cee73ee
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-09-06 14:39:20 +0200

    friday part 2 done

--- src/main/webapp/Fetch.js
+++ src/main/webapp/Fetch.js
@@ -15,7 +15,8 @@ document.querySelector("#getAllUsers").onclick = getAllUsers;
 function getUser(e) {
     e.preventDefault();
     var ID = document.querySelector("#userID").value;
-    let url = "https://jsonplaceholder.typicode.com/users/" + ID;
+    let url = "/review_week36_monday/api/movies/" + ID;
+//    let url = "http://localhost:8080/review_week36_monday/api/movies/" + ID;
     console.log(url);
     fetch(url)
             .then(res => res.json()) //in flow1, just do it
@@ -38,29 +39,30 @@ function getUser(e) {
 function getAllUsers(e) {
     e.preventDefault();
     let url = "/review_week36_monday/api/movies/all"; 
+//    let url = "http://localhost:8080/review_week36_monday/api/movies/all"; 
     console.log(url);
     fetch(url)
             .then(res => res.json()) //in flow1, just do it
             .then(data => {
                 // Inside this callback, and only here, the response data is available
-                console.log("data", data);
                 var table = [];
                 var name;
                 var phone;
+                console.log("data.list: "+ data.list);
                 table.push("<thead><tr><th>Title:</th><th>Year:</th></th></tr></thead>");
                 document.querySelector("#users").innerHTML = data.comp;
-                data.forEach(function (k) {
+                data.list.forEach(function (k) {
                     Object.keys(k).forEach(function (key) {
                         if (key === 'title') {
-                            name = k[key];
+                            title = k[key];
                             console.log("title: " + title);
                         }
                         if (key === 'year') {
-                            phone = k[key];
+                            year = k[key];
                             console.log("year: " + year);
                         }
                     });
-                    table.push("<tbody><tr><td>" + name + "</td><td>" + phone 
+                    table.push("<tbody><tr><td>" + title + "</td><td>" + year 
                             + "</td></tr></tbody>");
                 });
                 document.querySelector("#users").innerHTML =
